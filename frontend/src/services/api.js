bbef1faf4938b14d5055e4d8583b2988d364e4ca
NishantRajput1211 <nishantthakur1306@gmail.com>
2026-05-20 15:54:00 +0530

    Updated backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://team-task-tracker-agwp.onrender.com/api",
+  baseURL: "https://task-tracker-agwp.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
